a54836ce72d3412d151135ff2b5047de6fe999c3
Ktoxcon <kennethtoxro@gmail.com>
2020-06-11 15:46:06 -0600

    Command Parser Fixed

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,5 +1,6 @@
 exports.CommandParser = (req) => {
-    const toParse = req.body.command.trim().split(' ');
+    const toParse = req.body.command
+                    .trim().split(' ').filter(arg => arg.trim());
     const command = toParse.shift();
     const args = toParse;
     
@@ -26,61 +27,10 @@ exports.CommandMatcher = (reqCommand) => {
 
     const commandMatched = COMMANDS.filter((command) => {
         command === reqCommand.command.toUpperCase() ? command : undefined;
-    });
+    }).join();
 
     return commandMatched;
 }
 
 
-export const getAction = (command,args) => {
-    const commandResult = undefined;
-    
-    switch(command){
-        case "ADD_TWEET":
-            args.length === 1 ?
-                commandResult = getNewTweet(args)
-                : commandResult = "Invalid arguments";
-        break;
-        case "DELETE_TWEET":
-            args.length === 1 ?
-                commandResult = getTweetId(args)
-                : commandResult = "Invalid arguments";
-        break;
-        case "EDIT_TEEWT":
-            args.length === 2 ?
-                commandResult = getEditedTweet(args)
-                : commandResult = "Invalid Arguments";
-        break;
-    }
 
-    return action;
-}
-
-
-export const getNewTweet = (tweetContent) => {
-    return {
-        body:tweetContent[0],
-        creator:null,
-        date:null
-    };
-}
-
-export const getTweetId = (tweetId) => {
-    return {
-        id:tweetId[0]
-    };
-}
-
-
-export const getEditedTweet = (tweet) => {
-    return {
-        id:tweet[0],
-        body:tweet[1]
-    };
-}
-
-export const getTweets = (userName) => {
-    return{
-        username:userName[0]
-    };
-}
--- src/lib/util.test.js
+++ src/lib/util.test.js
@@ -1,24 +1,23 @@
 import { CommandParser } from "./util";
 
-// describe('Parsing Req for obtain commmands', () => {
-//    it('It should return a object with the command and the args', () => {
+ describe('Parsing Req for obtain commmands', () => {
+    it('It should return a object with the command and the args', () => {
 
-//        const req = { body:{command:"add arg arg"}};
-//        const req1 = { body:{command:"follow arg "}};
-//        const req2 = { body:{command:"login arg arg arg"}};
+        const req = { body:{command:"add arg arg"}};
+        const req1 = { body:{command:"follow arg "}};
+        const req2 = { body:{command:"login   arg arg arg"}};
+        const req3 = { body:{command:" login  arg  arg  arg "}};
 
-//        expect(CommandParser(req)).toEqual({command:"add",data:["arg","arg"]});
-//        expect(CommandParser(req1)).toEqual({command:"follow",data:["arg"]});
-//        expect(CommandParser(req2)).toEqual({command:"login",data:["arg","arg","arg"]});
-       
-//    });
-   
-
-// });
-
-describe('Matching Operations', () => {
-    it('It should return a ', () => {
-        
+        expect(CommandParser(req)).toEqual({command:"add",data:["arg","arg"]});
+        expect(CommandParser(req1)).toEqual({command:"follow",data:["arg"]});
+        expect(CommandParser(req2)).toEqual({command:"login",data:["arg","arg","arg"]});
+        expect(CommandParser(req3)).toEqual({command:"login",data:["arg","arg","arg"]});
     });
+ });
+
+// describe('Matching Operations', () => {
+//     it('It should return a ', () => {
+        
+//     });
     
-});
+// });
