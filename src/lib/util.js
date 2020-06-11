d160ce505bff9521362b62f621e0e838eb558d58
Ktoxcon <kennethtoxro@gmail.com>
2020-06-11 17:42:13 -0600

    Argument Validator Added

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,13 +3,8 @@ module.exports.CommandParser = (req) => {
                     .trim().split(' ').filter(arg => arg.trim());
     const command = toParse.shift();
     const args = toParse;
-    
-    const parsed = {
-        command:command,
-        data:args
-    };
-    
-    return parsed;
+
+    return {command,args};
 }
 
 exports.CommandMatcher = ({command}) => {
@@ -37,5 +32,30 @@ exports.CommandMatcher = ({command}) => {
 
 }
 
+exports.ArgumentMatcher = ({command,args}) => {
+    const COMMANDS = {
+        "ADD_TWEET":3,
+        "DELETE_TWEET":1,
+        "EDIT_TWEET":2,
+        "VIEW_TWEETS":0 || 1,
+        "FOLLOW":1,
+        "UNFOLLOW":1,
+        "PROFILE":1,
+        "LOGIN":2,
+        "REGISTER":4
+    };
 
+    let validArgs = false;
+
+    for(const commandMatched in COMMANDS){
+        if(String(command).toUpperCase() === commandMatched){
+            if(args.length === COMMANDS[commandMatched]){
+                validArgs = true;
+            }
+        }
+    }
+
+    return validArgs;
+
+}
 
